562c3c1b8455deddb26cdec5294a7c08bedb382f
ebookcode123 <ebookcode123@gmail.com>
2017-03-30 09:19:19 -0700

    add chart example

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 // import {bindActionCreators} from 'redux';
 // import {fetchWeather} from '../actions/index';
 
 class WeatherList extends Component {
  renderWeather(cityData) {
+   const name = cityData.city.name;
+   const temps = cityData.list.map(weather => weather.main.temp);
+   const pressures = cityData.list.map(weather => weather.main.pressure);
+   const humidities = cityData.list.map(weather => weather.main.humidity);
+  //  console.log(temps);
    return (
-     <tr >
-      <td>{cityData.city.name}</td>
+     <tr key = {name}>
+      <td>{name}</td>
+      <td><Chart data={temps} color="orange" /></td>
+      <td><Chart data={pressures} color="blue" /></td>
+      <td><Chart data={humidities} color="black" /></td>
      </tr>
    )
  }
