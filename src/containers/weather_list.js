0ef6e7944fab4678cfba9a1f9d1fd3fdf753409f
ebookcode123 <ebookcode123@gmail.com>
2017-03-31 15:48:35 -0700

    add mean line

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -15,9 +15,9 @@ class WeatherList extends Component {
    return (
      <tr key = {name}>
       <td>{name}</td>
-      <td><Chart data={temps} color="orange" /></td>
-      <td><Chart data={pressures} color="blue" /></td>
-      <td><Chart data={humidities} color="black" /></td>
+      <td><Chart data={temps} color="orange" units ="K"/></td>
+      <td><Chart data={pressures} color="blue" units="hPa"/></td>
+      <td><Chart data={humidities} color="black" units="%"/></td>
      </tr>
    )
  }
@@ -28,9 +28,9 @@ class WeatherList extends Component {
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature (K)</th>
+            <th>Pressure (hPa)</th>
+            <th>Humidity (%)</th>
           </tr>
         </thead>
         <tbody>
