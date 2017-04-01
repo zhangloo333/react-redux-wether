ec50b920e996748ea7dd2d885c7d8cc3fa7466d2
ebookcode123 <ebookcode123@gmail.com>
2017-03-31 17:08:41 -0700

    finish weather project

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 // import {bindActionCreators} from 'redux';
 // import {fetchWeather} from '../actions/index';
 
@@ -11,10 +12,13 @@ class WeatherList extends Component {
    const temps = cityData.list.map(weather => weather.main.temp);
    const pressures = cityData.list.map(weather => weather.main.pressure);
    const humidities = cityData.list.map(weather => weather.main.humidity);
+   const lon = cityData.city.coord.lon;
+   const lat = cityData.city.coord.lat;
   //  console.log(temps);
+
    return (
      <tr key = {name}>
-      <td>{name}</td>
+      <td><GoogleMap lon={lon} lat={lat}/></td>
       <td><Chart data={temps} color="orange" units ="K"/></td>
       <td><Chart data={pressures} color="blue" units="hPa"/></td>
       <td><Chart data={humidities} color="black" units="%"/></td>
